f3fff5195d6bd628634f3f80b95992c92b286c4f
inakibuj <inakibujperis@gmail.com>
2025-12-09 18:58:51 +0100

    dbt_a_punta_pala

---
+++ dbt/models/staging/stg_nyc_911_calls.sql
@@ -0,0 +1,40 @@
+with source as (
+
+    select * from {{ source('raw_nyc_data', 'emergency_calls') }}
+
+),
+
+renamed as (
+
+    select
+        -- IDs y Claves
+        cad_evnt_id as event_id,
+        
+        -- Fechas y Tiempos
+        create_date as created_at,
+        incident_date,
+        incident_time, -- Podrías intentar combinarlos si incident_date no tiene hora
+        add_ts as added_at,
+        disp_ts as dispatched_at,
+        arrivd_ts as arrived_at,
+        closng_ts as closed_at,
+
+        -- Detalles del Incidente
+        typ_desc as incident_type_description,
+        radio_code,
+        cip_jobs as job_status, -- Critical Injury / Police jobs
+        
+        -- Ubicación
+        boro_nm as borough,
+        patrl_boro_nm as patrol_borough,
+        nypd_pct_cd as precinct_id,
+        latitude,
+        longitude,
+        geo_cd_x,
+        geo_cd_y
+
+    from source
+
+)
+
+select * from renamed
