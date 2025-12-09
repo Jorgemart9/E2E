239ac4e40b7a2fc9199e09dfc3aa93ca0c7c6170
cabesaEDEM <cabesa@edem.es>
2025-12-09 16:37:02 +0100

    database init

---
+++ database/init.sql
@@ -0,0 +1,23 @@
+\c new_york;
+
+
+CREATE TABLE icad_events (
+    cad_evnt_id        TEXT PRIMARY KEY,
+    create_date        TIMESTAMP,
+    incident_date      TIMESTAMP,
+    incident_time      TEXT,
+    nypd_pct_cd        INTEGER,
+    boro_nm            TEXT,
+    patrl_boro_nm      TEXT,
+    geo_cd_x           TEXT,
+    geo_cd_y           TEXT,
+    radio_code         TEXT,
+    typ_desc           TEXT,
+    cip_jobs           TEXT,
+    add_ts             TIMESTAMP,
+    disp_ts            TIMESTAMP,
+    arrivd_ts          TIMESTAMP,
+    closng_ts          TIMESTAMP,
+    latitude           DOUBLE PRECISION,
+    longitude          DOUBLE PRECISION
+);
