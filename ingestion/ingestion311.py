b692d3e2db0a97f9231cf8245cee26b12bf28f63
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-09 17:50:56 +0100

    cambios ingestino 311

--- ingestion/ingestion311.py
+++ ingestion/ingestion311.py
@@ -4,7 +4,20 @@ import os
 import io
 import pandas as pd
 
-url_datos = "https://data.cityofnewyork.us/resource/n2zq-pubd.json"
+url_datos = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
 
 url_api = os.getenv("API_URL", "http://api:5000") + "/ingest/911_calls"
 
+cont = 0
+def procesar_datos(cont):
+    print("Descargando datos...")
+    try:
+        resp = requests.get(url_datos)
+        data = resp.json()
+    except Exception as e:
+        print(f"Error descargando: {e}")
+        return
+    lista_inserciones = []
+    for datos in data:
+        try:
+            
