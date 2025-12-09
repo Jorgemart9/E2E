0b88d845cd38aed93055162b8f2275d3b3305790
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-09 16:51:24 +0100

    cambios finales

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -39,26 +39,26 @@ def procesar_datos(cont):
             longitude = datos["longitude"]
 
             fila = {
-                'CAD_EVNT_ID': cad_evnt_id,
-                'CREATE_DATE': create_date,
-                'INCIDENT_DATE': incident_date,
-                'INCIDENT_TIME': incident_time,
-                'NYPD_PCT_CD': nypd_pct_cd,
-                'BORO_NM': boro_nm,
-                'PATRL_BORO_NM': patrl_boro_nm,
-                'GEO_CD_X': geo_cd_x,
-                'GEO_CD_Y': geo_cd_y,
-                'RADIO_CODE': radio_code,
-                'TYPE_DESC': typ_desc,
-                'ZIP_JOBS': zip_jobs,
-                'ADD_TS': add_ts,
-                'DISP_TS': disp_ts,
-                'ARRIVD_TS': arrivd_ts,
-                'CLOSNG_TS': closng_ts,
-                'LATITUDE': latitude,
-                'LONGITUDE': longitude
+                'cad_envt_id': cad_evnt_id,
+                'create_date': create_date,
+                'incident_date': incident_date,
+                'incident_time': incident_time,
+                'nypd_pct_cd': nypd_pct_cd,
+                'boro_nm': boro_nm,
+                'patrl_boro_nm': patrl_boro_nm,
+                'geo_cd_x': geo_cd_x,
+                'geo_cd_y': geo_cd_y,
+                'radio_code': radio_code,
+                'type_desc': typ_desc,
+                'zip_jobs': zip_jobs,
+                'add_ts': add_ts,
+                'disp_ts': disp_ts,
+                'arrivd_ts': arrivd_ts,
+                'closng_ts': closng_ts,
+                'latitude': latitude,
+                'longitude': longitude
             }
             lista_inserciones.append(fila)
-    except Exception as e:
-        print(f"Error procesando datos: {e}")
+    except ValueError:
+        continue
 
